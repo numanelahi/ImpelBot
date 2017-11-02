2ebc7f870ca7455ec8e398b2937728d2921532ed
Numan Elahi <elahinouman.elahi@gmail.com>
2017-11-02 21:38:45 +0530

    Insecuring the credentials

--- src/app.js
+++ src/app.js
@@ -8,6 +8,5 @@ const bot = new TelegramBot(TOKEN, {webHook: {port: 8000, host: 'localhost'}});
 bot.setWebHook(DOMAIN+TOKEN);
 
 bot.on('message', msg => {
-       console.log('This works', JSON.stringify(msg));
        bot.sendMessage(msg.chat.id, 'Asalamualaikum');
 });
