4aa8717ac9bed0f4eb5d1a387cdf22416b085675
Numan Elahi <elahinouman.elahi@gmail.com>
2017-11-02 20:09:04 +0530

    Experiment

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
+import fs from 'fs';
+import TelegramBot from 'node-telegram-bot-api'
+import path from 'path';
 import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-       res.send('Asalamualaikum');
+const TOKEN = '484935789:AAEjnuJwEMC4xDLywoQtUG6yV5x4ogEU1nA',
+       DOMAIN = 'https://learnawsnow.com/';
+
+const bot = new TelegramBot(TOKEN)
+
+bot.setWebHook(DOMAIN+TOKEN, fs.createReadStream(path.join(__dirname, '/certs/crt.pem')));
+
+app.get(`/${TOKEN}`, (req, res) => {
+       console.log('Message received');
+       res.sendStatus(204)
 })
 
 export {app as default};
-
