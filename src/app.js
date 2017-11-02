53e96c67523f9c1f41bd06c4c32eda7695054f20
Numan Elahi <elahinouman.elahi@gmail.com>
2017-11-02 21:32:04 +0530

    Insecuring the credentials

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import TelegramBot from 'node-telegram-bot-api'
 
-const TOKEN = process.env.TOKEN,
-       DOMAIN = process.env.DOMAIN;
+const TOKEN = '484935789:AAEjnuJwEMC4xDLywoQtUG6yV5x4ogEU1nA',
+       DOMAIN = 'https://leanawsnow.com';
 
-const bot = new TelegramBot(TOKEN, {webHook: {port: process.env.PORT, host: process.env.HOST}});
+const bot = new TelegramBot(TOKEN, {webHook: {port: 8000, host: 'localhost'}});
 bot.setWebHook(`${DOMAIN}/bot${TOKEN}`);
 
 bot.on('message', msg => {
