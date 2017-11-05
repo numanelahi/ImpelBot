ab60f4aea778ca641166fb5e8e8c3d6c55e0642d
Numan Elahi <elahinouman.elahi@gmail.com>
2017-11-05 16:26:39 +0530

    Removing the credentials from the source code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api'
 
-const TOKEN = '484935789:AAEjnuJwEMC4xDLywoQtUG6yV5x4ogEU1nA',
-       DOMAIN = 'https://learnawsnow.com/bot';
-
 const bot = new TelegramBot(TOKEN, {webHook: {port: 8000, host: 'localhost'}});
 bot.setWebHook(DOMAIN+TOKEN);
 
