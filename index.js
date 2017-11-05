9cbfe35d64b5382d23fe5c5fb4447e2c6ef3e872
Numan Elahi <elahinouman.elahi@gmail.com>
2017-11-05 16:24:19 +0530

    Adding babel packages to transpile the code

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const path = require('path')
+
+// Requiring the babel packages to
+// transpile the ES6 code to ES5
 require('babel-register')
 require('babel-polyfill')
+
 require(path.join(__dirname, '/src/app.js'))
